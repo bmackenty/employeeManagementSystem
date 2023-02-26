fce5c80b66ffe9f60d8c06016439072de3aa0066
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-02-26 21:50:14 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Employee Management System</title>
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+    <div class="container">
+
+    <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Employee Management System</h1>
+        <p class="lead text-muted">This is an employee management system, where users can add, edit, and manage employees. 
+            It is designed to be used as a template to develop other applications which might need employee functionality.
+            </p>
+        <p>
+          <a href="features.php" class="btn btn-primary my-2">Features</a>
+          <a href="https://github.com/bmackenty/employeeManagementSystem" class="btn btn-primary my-2">Github</a>
+          <a href="setup.php" class="btn btn-primary my-2">Setup</a>
+        </p>
+      </div>
+    </div>
+  </section>
+
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
