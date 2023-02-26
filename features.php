eaa0c74789e18e7a6585bb014c0bed976a12680d
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-02-26 21:51:00 +0100

    Create features.php

---
+++ features.php
@@ -0,0 +1,101 @@
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <title>Employee Management System</title>
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+    <div class="container">
+
+
+
+        <div class="b-example-divider"></div>
+
+        <div class="container px-4 py-5" id="icon-grid">
+            <h2 class="pb-2 border-bottom">Features</h2>
+
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+
+                        <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/bootstrap.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                        <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/cpu-fill.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/calendar3.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/house.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/speedometer2.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/toggles2.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/geo-fill.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+                <div class="col d-flex align-items-start">
+                    <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em">
+                    <img class="m-2" height="40" width="50" src="bootstrap-icons-1.10.3/tools.svg">
+                    </svg>
+                    <div>
+                        <h4 class="fw-bold mb-0">Featured title</h4>
+                        <p>Paragraph of text beneath the heading to explain the heading.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
