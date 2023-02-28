6ff994846eb6b422f38b8a06bbab1146d1ee18fd
bmackenty <bmackenty@gmail.com>
2023-02-28 10:45:45 +0100

    cleaning up the code

--- login.php
+++ login.php
@@ -24,24 +24,9 @@
                         <label class="form-label" for="form2Example2">Password</label>
                     </div>
 
-                    <!-- 2 column grid layout for inline styling -->
-                    <div class="row mb-4">
-                        <div class="col d-flex justify-content-center">
-                            <!-- Checkbox -->
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                                <label class="form-check-label" for="form2Example31"> Remember me </label>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <!-- Simple link -->
-                            <a href="#!">Forgot password?</a>
-                        </div>
-                    </div>
 
                     <!-- Submit button -->
-                    <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+                    <button type="submit" class="btn btn-primary  w-100 mb-4">Sign in</button>
                     <div class="text-center">
                         <p>Not a member? <a href="register.php">Register</a></p>
 
