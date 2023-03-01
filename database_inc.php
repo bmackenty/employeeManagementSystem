490cca04fe7f28f2b1477790c78165cade36bf86
bmackenty <bmackenty@gmail.com>
2023-03-01 12:55:06 +0100

    now uses oop

--- database_inc.php
+++ database_inc.php
@@ -9,7 +9,7 @@
 // you need to replace YOURUSERNAME with you username. You need to do this twice
 // you need to replace YOURPASSWORD with your password. You only need to do this once.
 // 
-// There is a conditional statement on line 17 which, if $connect is NOT TRUE
+// There is a conditional statement on line 20 which, if $connect is NOT TRUE
 // prints an error message helping us understand the reason why we could not connect 
 // to the database
 
